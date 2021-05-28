cf6a127a1a1bd005a6286d9d48d5ae46102fc064
adnanrasoolgohar <adnanrasool1@outlook.com>
2021-05-28 09:46:58 +0500

    login

--- TPA/Login.Designer.cs
+++ TPA/Login.Designer.cs
@@ -38,19 +38,27 @@
             this.telerikMetroBlueTheme1 = new Telerik.WinControls.Themes.TelerikMetroBlueTheme();
             this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
             this.radPanel2 = new Telerik.WinControls.UI.RadPanel();
-            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
-            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
-            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
-            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
             this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radTextBox3 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel4 = new Telerik.WinControls.UI.RadLabel();
             ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radPanel2)).BeginInit();
             this.radPanel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,6 +73,10 @@
             // 
             // radPanel2
             // 
+            this.radPanel2.Controls.Add(this.radLabel4);
+            this.radPanel2.Controls.Add(this.radTextBox3);
+            this.radPanel2.Controls.Add(this.radButton2);
+            this.radPanel2.Controls.Add(this.radButton1);
             this.radPanel2.Controls.Add(this.radTextBox2);
             this.radPanel2.Controls.Add(this.radTextBox1);
             this.radPanel2.Controls.Add(this.radLabel3);
@@ -77,6 +89,44 @@
             this.radPanel2.TabIndex = 1;
             this.radPanel2.ThemeName = "TelerikMetroTouch";
             // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Location = new System.Drawing.Point(97, 158);
+            this.radTextBox2.Name = "radTextBox2";
+            this.radTextBox2.Size = new System.Drawing.Size(300, 32);
+            this.radTextBox2.TabIndex = 3;
+            this.radTextBox2.ThemeName = "TelerikMetroTouch";
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(97, 119);
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.Size = new System.Drawing.Size(300, 32);
+            this.radTextBox1.TabIndex = 2;
+            this.radTextBox1.ThemeName = "TelerikMetroTouch";
+            // 
+            // radLabel3
+            // 
+            this.radLabel3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel3.Location = new System.Drawing.Point(9, 158);
+            this.radLabel3.Name = "radLabel3";
+            this.radLabel3.Size = new System.Drawing.Size(63, 21);
+            this.radLabel3.TabIndex = 1;
+            this.radLabel3.Text = "Password";
+            this.radLabel3.ThemeName = "TelerikMetroTouch";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.AutoSize = false;
+            this.radLabel2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.Location = new System.Drawing.Point(9, 119);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(82, 29);
+            this.radLabel2.TabIndex = 1;
+            this.radLabel2.Text = "Username";
+            this.radLabel2.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.radLabel2.ThemeName = "TelerikMetroTouch";
+            // 
             // radLabel1
             // 
             this.radLabel1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -87,45 +137,45 @@
             this.radLabel1.Text = "Login";
             this.radLabel1.ThemeName = "TelerikMetroTouch";
             // 
-            // radLabel2
+            // radButton1
             // 
-            this.radLabel2.AutoSize = false;
-            this.radLabel2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLabel2.Location = new System.Drawing.Point(6, 146);
-            this.radLabel2.Name = "radLabel2";
-            this.radLabel2.Size = new System.Drawing.Size(208, 29);
-            this.radLabel2.TabIndex = 1;
-            this.radLabel2.Text = "Username";
-            this.radLabel2.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
-            this.radLabel2.ThemeName = "TelerikMetroTouch";
+            this.radButton1.Location = new System.Drawing.Point(128, 234);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 32);
+            this.radButton1.TabIndex = 4;
+            this.radButton1.Text = "Login";
+            this.radButton1.ThemeName = "Crystal";
             // 
-            // radLabel3
+            // radButton2
             // 
-            this.radLabel3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLabel3.Location = new System.Drawing.Point(6, 196);
-            this.radLabel3.Name = "radLabel3";
-            this.radLabel3.Size = new System.Drawing.Size(63, 21);
-            this.radLabel3.TabIndex = 1;
-            this.radLabel3.Text = "Password";
-            this.radLabel3.ThemeName = "TelerikMetroTouch";
+            this.radButton2.Location = new System.Drawing.Point(244, 234);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(110, 32);
+            this.radButton2.TabIndex = 5;
+            this.radButton2.Text = "Exit";
+            this.radButton2.ThemeName = "Crystal";
             // 
-            // radTextBox1
+            // radTextBox3
             // 
-            this.radTextBox1.Location = new System.Drawing.Point(94, 146);
-            this.radTextBox1.Name = "radTextBox1";
-            this.radTextBox1.Size = new System.Drawing.Size(300, 32);
-            this.radTextBox1.TabIndex = 2;
-            this.radTextBox1.ThemeName = "TelerikMetroTouch";
+            this.radTextBox3.Location = new System.Drawing.Point(97, 196);
+            this.radTextBox3.Name = "radTextBox3";
+            this.radTextBox3.Size = new System.Drawing.Size(300, 32);
+            this.radTextBox3.TabIndex = 4;
+            this.radTextBox3.ThemeName = "TelerikMetroTouch";
             // 
-            // radTextBox2
+            // radLabel4
             // 
-            this.radTextBox2.Location = new System.Drawing.Point(94, 185);
-            this.radTextBox2.Name = "radTextBox2";
-            this.radTextBox2.Size = new System.Drawing.Size(300, 32);
-            this.radTextBox2.TabIndex = 3;
-            this.radTextBox2.ThemeName = "TelerikMetroTouch";
+            this.radLabel4.AutoSize = false;
+            this.radLabel4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel4.Location = new System.Drawing.Point(9, 196);
+            this.radLabel4.Name = "radLabel4";
+            this.radLabel4.Size = new System.Drawing.Size(82, 29);
+            this.radLabel4.TabIndex = 2;
+            this.radLabel4.Text = "Level";
+            this.radLabel4.TextAlignment = System.Drawing.ContentAlignment.TopLeft;
+            this.radLabel4.ThemeName = "TelerikMetroTouch";
             // 
-            // Login1
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -133,7 +183,7 @@
             this.Controls.Add(this.radPanel2);
             this.Controls.Add(this.radPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Login1";
+            this.Name = "Login";
             // 
             // 
             // 
@@ -144,11 +194,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.radPanel2)).EndInit();
             this.radPanel2.ResumeLayout(false);
             this.radPanel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
@@ -171,5 +225,9 @@
         private Telerik.WinControls.UI.RadLabel radLabel2;
         private Telerik.WinControls.UI.RadTextBox radTextBox2;
         private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
+        private Telerik.WinControls.UI.RadTextBox radTextBox3;
     }
 }
