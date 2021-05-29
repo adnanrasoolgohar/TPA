ae5d1d837a651e1603b032990cec1f75703ac906
adnanrasoolgohar <adnanrasool1@outlook.com>
2021-05-29 09:57:10 +0500

    accsetup

---
+++ TPA/AccountSetup.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace TPA
+{
+    partial class AccountSetup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition2 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.radPageView1 = new Telerik.WinControls.UI.RadPageView();
+            this.radPageViewPage1 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPageViewPage2 = new Telerik.WinControls.UI.RadPageViewPage();
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.radPanel2 = new Telerik.WinControls.UI.RadPanel();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.radVirtualGrid1 = new Telerik.WinControls.UI.RadVirtualGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.radPageView1)).BeginInit();
+            this.radPageView1.SuspendLayout();
+            this.radPageViewPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.radPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel2)).BeginInit();
+            this.radPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radVirtualGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radPageView1
+            // 
+            this.radPageView1.Controls.Add(this.radPageViewPage1);
+            this.radPageView1.Controls.Add(this.radPageViewPage2);
+            this.radPageView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radPageView1.Location = new System.Drawing.Point(0, 0);
+            this.radPageView1.Name = "radPageView1";
+            this.radPageView1.SelectedPage = this.radPageViewPage1;
+            this.radPageView1.Size = new System.Drawing.Size(1186, 689);
+            this.radPageView1.TabIndex = 0;
+            this.radPageView1.ThemeName = "TelerikMetroTouch";
+            // 
+            // radPageViewPage1
+            // 
+            this.radPageViewPage1.Controls.Add(this.radPanel2);
+            this.radPageViewPage1.Controls.Add(this.radPanel1);
+            this.radPageViewPage1.ItemSize = new System.Drawing.SizeF(117F, 29F);
+            this.radPageViewPage1.Location = new System.Drawing.Point(5, 40);
+            this.radPageViewPage1.Name = "radPageViewPage1";
+            this.radPageViewPage1.Size = new System.Drawing.Size(1176, 644);
+            this.radPageViewPage1.Text = "Detail Accounts";
+            // 
+            // radPageViewPage2
+            // 
+            this.radPageViewPage2.ItemSize = new System.Drawing.SizeF(113F, 29F);
+            this.radPageViewPage2.Location = new System.Drawing.Point(5, 40);
+            this.radPageViewPage2.Name = "radPageViewPage2";
+            this.radPageViewPage2.Size = new System.Drawing.Size(1176, 644);
+            this.radPageViewPage2.Text = "Party Accounts";
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.Controls.Add(this.radGridView1);
+            this.radPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.radPanel1.Location = new System.Drawing.Point(0, 0);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(495, 644);
+            this.radPanel1.TabIndex = 0;
+            // 
+            // radPanel2
+            // 
+            this.radPanel2.Controls.Add(this.radVirtualGrid1);
+            this.radPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radPanel2.Location = new System.Drawing.Point(495, 0);
+            this.radPanel2.Name = "radPanel2";
+            this.radPanel2.Size = new System.Drawing.Size(681, 644);
+            this.radPanel2.TabIndex = 1;
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radGridView1.Location = new System.Drawing.Point(0, 0);
+            // 
+            // 
+            // 
+            this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition2;
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.Size = new System.Drawing.Size(495, 644);
+            this.radGridView1.TabIndex = 0;
+            // 
+            // radVirtualGrid1
+            // 
+            this.radVirtualGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radVirtualGrid1.Location = new System.Drawing.Point(0, 0);
+            this.radVirtualGrid1.Name = "radVirtualGrid1";
+            this.radVirtualGrid1.Size = new System.Drawing.Size(681, 644);
+            this.radVirtualGrid1.TabIndex = 0;
+            // 
+            // AccountSetup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 689);
+            this.Controls.Add(this.radPageView1);
+            this.Name = "AccountSetup";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "FINANCIAL SETUP";
+            this.ThemeName = "TelerikMetroTouch";
+            ((System.ComponentModel.ISupportInitialize)(this.radPageView1)).EndInit();
+            this.radPageView1.ResumeLayout(false);
+            this.radPageViewPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.radPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel2)).EndInit();
+            this.radPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radVirtualGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPageView radPageView1;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage1;
+        private Telerik.WinControls.UI.RadPageViewPage radPageViewPage2;
+        private Telerik.WinControls.UI.RadPanel radPanel2;
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private Telerik.WinControls.UI.RadGridView radGridView1;
+        private Telerik.WinControls.UI.RadVirtualGrid radVirtualGrid1;
+    }
+}
---
+++ TPA/AccountSetup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace TPA
+{
+    public partial class AccountSetup : Telerik.WinControls.UI.RadForm
+    {
+        public AccountSetup()
+        {
+            InitializeComponent();
+            Set_Grid();
+        }
+        private  void Set_Grid()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID", typeof(int)).DefaultValue = 0;
+            dt.Columns.Add("Name1", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name2", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name3", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name4", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name5", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name6", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name7", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name8", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name9", typeof(string)).DefaultValue = "";
+            dt.Columns.Add("Name10", typeof(string)).DefaultValue = "";
+
+            dt.PrimaryKey = new DataColumn[] { dt.Columns["ID"] };
+
+
+            for (int i = 1; i <= 4000000; i++)
+            {
+                DataRow row = dt.NewRow();
+                row[0] = i;
+                row[1] = "Name1";
+                row[2] = "Name2";
+                row[3] = "Name3";
+                row[4] = "Name4";
+                row[5] = "Name5";
+                row[6] = "Name6";
+                row[7] = "Name7";
+                row[8] = "Name8";
+                row[9] = "Name9";
+                row[10] = "Name10";
+
+                dt.Rows.Add(row);
+
+            }
+            radGridView1.DataSource = dt;
+            //radVirtualGrid1.p = dt;
+
+
+        }
+    }
+}
